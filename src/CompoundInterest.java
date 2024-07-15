e74a0c9ad3f0c521bb7dc8a08c2498ba5d5fd03a
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-15 18:11:00 -0230

    update

--- src/CompoundInterest.java
+++ src/CompoundInterest.java
@@ -10,7 +10,7 @@ public class CompoundInterest {
         for (int rateCounter = 5; rateCounter <= 10; rateCounter++) {
             for (int yearCounter = 1; yearCounter <= 7; yearCounter++) {
                 double saving = principal * Math.pow(1 + (double)rateCounter/100, yearCounter);//if dont cast double, results in integer division,
-                //integer division gets 1. i to the nth power is still 1. After many years' saving, your money stays the same!
+                //integer division gets 1. 1 to the nth power is still 1. After many years' saving, your money stays the same!
                 System.out.printf("%nWith a rate of %d%%, the %d th year's saving is %.2f", rateCounter, yearCounter, saving);
             }
             System.out.println();
