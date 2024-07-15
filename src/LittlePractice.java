d1bc4b2a4599974601479e2c97dbbb5ac6c67bb0
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-15 20:40:12 -0230

    update

---
+++ src/LittlePractice.java
@@ -0,0 +1,15 @@
+/**
+ * This class prints some output for practice
+ */
+public class LittlePractice {
+    public static void main(String[] args) {
+        int i = 1;
+        int j =2;
+        int k = 3;
+        int m = 2;
+        System.out.println(i == 1);//what does this print?
+        //above prints ---   true
+
+
+    }
+}
