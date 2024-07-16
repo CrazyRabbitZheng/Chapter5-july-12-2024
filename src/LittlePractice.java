d3799f3187b1af49ee3b464431f58270244dac6d
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-15 23:58:52 -0230

    update

--- src/LittlePractice.java
+++ src/LittlePractice.java
@@ -11,5 +11,28 @@ public class LittlePractice {
         //above prints ---   true
 
 
+        for (i =1; i<=5; i++){
+            for (j =1; j<=3; j++){
+                for (k =1; k<=4; k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        /**
+         * code above prints
+         * <pre>
+         * ****
+         * ****
+         * ****
+         *
+         *(3 more groups here)
+         *
+         * ****
+         * ****
+         * ****
+         * </pre>
+         */
     }
 }
